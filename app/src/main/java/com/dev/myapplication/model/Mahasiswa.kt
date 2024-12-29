006467efcc56afbae5955f836132c2cf890b355d
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-29 22:57:52 +0700

    memperbaiki masalah serialisasi pada jenisKelamin dengan menambahkan @SerialName

--- app/src/main/java/com/dev/myapplication/model/Mahasiswa.kt
+++ app/src/main/java/com/dev/myapplication/model/Mahasiswa.kt
@@ -1,12 +1,16 @@
 package com.dev.myapplication.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Mahasiswa (
     val nim: String,
     val nama: String,
+
+    @SerialName("jenis_kelamin")
     val jenisKelamin: String,
+
     val alamat: String,
     val kelas: String,
     val angkatan: String
