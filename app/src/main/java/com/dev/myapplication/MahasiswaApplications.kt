f12d4410729c8a901d76bced22501eba93932393
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-30 08:18:09 +0700

    menambahkan MahasiswaApplications sebagai kelas Application untuk mengelola dependensi aplikasi

---
+++ app/src/main/java/com/dev/myapplication/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.dev.myapplication
+
+import android.app.Application
+import com.dev.myapplication.container.AppContainer
+import com.dev.myapplication.container.MahasiswaContainer
+
+class MahasiswaApplications: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
